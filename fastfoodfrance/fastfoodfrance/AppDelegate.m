bf433e40a9d13e76a768130ee7821e0b64e3abeb
tiwence <terence.marill@gmail.com>
2013-06-06 18:40:00 +0200

    Started point for UINavigationController set

--- fastfoodfrance/fastfoodfrance/AppDelegate.m
+++ fastfoodfrance/fastfoodfrance/AppDelegate.m
@@ -17,7 +17,6 @@
 {
     //self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
     [self.window addSubview:self.navController.view];
     [self.window makeKeyAndVisible];
     return YES;
